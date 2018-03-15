05e876f1ffea8b79e344211610fdcd4db32f2f4b
Arthur Sena <arthur.psena@gmail.com>
2018-03-15 08:08:40 -0300

    Aula 68
    Componente para finalizar uma compra.

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from "@angular/core";
 import { ShoppingCartService } from "../restaurant-detail/shopping-cart/shopping-cart.service";
 import { CartItem } from "../restaurant-detail/shopping-cart/cart-item.model";
+import { Order, OrderItem } from "./order.model";
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/operator/map'
+import { Http, RequestOptions, Headers } from "@angular/http";
+
+import {MEAT_API} from '../app.api'
 
 @Injectable()
 export class OrderService {
 
-      constructor(private carteService: ShoppingCartService){
+    constructor(private carteService: ShoppingCartService, private http: Http){
 
     }
 
@@ -27,6 +33,18 @@ export class OrderService {
 
     itemsValue(): number {
        return this.carteService.total()
+    }
+
+    checkOrder(order: Order): Observable<string> {
+        const headers = new Headers()
+        headers.append('Content-Type', 'application/json')
+        return this.http.post(`${MEAT_API}/orders`, 
+                    JSON.stringify(order), new RequestOptions({headers: headers}))
+                    .map(response => response.json())
+    }
+
+    clear(): any {
+        this.carteService.clear()
     }
 
 }
