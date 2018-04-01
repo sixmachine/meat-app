e1438de482a06c76eb5f7acc08cb1fff297eefff
Unknown <arthur_psena@hotmail.com>
2018-04-01 17:46:40 -0300

    Aula 69
    Criado rota e maneira de navegação de dentro de um componente e não por routerlink

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -3,6 +3,7 @@ import {RadioOption} from '../shared/radio/radio-option.model'
 import { OrderService } from './order.service';
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
 import { Order, OrderItem } from './order.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'mt-order',
@@ -18,7 +19,7 @@ export class OrderComponent implements OnInit {
     {label : "Cartão Refeição", value : "REF"},
   ]
 
-  constructor(private orderService: OrderService) { }
+  constructor(private orderService: OrderService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -51,6 +52,7 @@ export class OrderComponent implements OnInit {
           new OrderItem(item.quantity, item.menuItem.id))
     this.orderService.checkOrder(order)
     .subscribe((orderId: string) => {
+      this.router.navigate(['/order-summary'])
       console.log(`Compra concluída: ${orderId}`)
       this.orderService.clear()
     })
--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -41,6 +41,7 @@ export class OrderService {
         return this.http.post(`${MEAT_API}/orders`, 
                     JSON.stringify(order), new RequestOptions({headers: headers}))
                     .map(response => response.json())
+                    .map(order => order.id)
     }
 
     clear(): any {
