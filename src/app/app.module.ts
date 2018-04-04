285218c7e4035b11f99aa29f0e853f8e82a2e6be
Unknown <arthur_psena@hotmail.com>
2018-04-03 22:57:33 -0300

    Aula 72
    Removi a diretiva ngModel para usar ReactiveForms. Usei FormControl, FormGroup e adicionei no providers da aplicação o modulo ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-import {FormsModule} from '@angular/forms'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 import {ROUTES} from './app.routes';
 import { AppComponent } from './app.component';
@@ -53,6 +53,7 @@ import { RatingComponent } from './shared/rating/rating.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [RestaurantsService, ShoppingCartService, {provide: LOCALE_ID, useValue: 'pt-BR'}, OrderService],
