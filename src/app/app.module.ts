3f5f8ab2c0b8dff5c90afb3bdff27f31f9dc33fc
Unknown <arthur_psena@hotmail.com>
2018-04-07 20:57:23 -0300

    Aula 80
    Refactory do código para remoção do core module e criação dos providers de serviço no shared-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,6 @@ import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.compo
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core/core.module';
 
 
 
@@ -39,8 +38,7 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     FormsModule,
     HttpModule,
-    SharedModule,
-    CoreModule,
+    SharedModule.forRoot(),
     ReactiveFormsModule,
     RouterModule.forRoot(ROUTES)
   ],
