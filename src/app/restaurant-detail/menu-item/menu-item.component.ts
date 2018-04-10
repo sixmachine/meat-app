15bbc26639c80cfd5db02276b16f849aee81038d
Unknown <arthur_psena@hotmail.com>
2018-04-10 00:10:12 -0300

    Aula 89
    Apliquei uma animação semelhante ao que foi aplicado ao componente de restaurantes.

--- src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
-import {MenuItem} from './menu-item.model'
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { MenuItem } from './menu-item.model'
 
 @Component({
   selector: 'mt-menu-item',
   templateUrl: './menu-item.component.html',
+  animations: [
+    trigger('menuItemAppeared', [
+      state('ready', style({ opacity: 1 })),
+      transition('void => ready', [
+        style({ opacity: 0, transform: 'translateY(-20px)' }),
+        animate('300ms 0s ease-in')
+      ])
+
+    ])
+  ]
 })
 export class MenuItemComponent implements OnInit {
+
+  menuItemState = 'ready'
 
   @Input() menuItem: MenuItem
   @Output() add = new EventEmitter()
@@ -15,7 +28,7 @@ export class MenuItemComponent implements OnInit {
   ngOnInit() {
   }
 
-  emitAddEvent(){
+  emitAddEvent() {
     this.add.emit(this.menuItem)
   }
 }
