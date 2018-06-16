a30cf6b5f4980eeb64f051b2400dcbe22843df94
Unknown <arthur_psena@hotmail.com>
2018-06-16 14:08:26 -0300

    Aula 115
    Criei um arquivo de autorização e configurei o servidor para proteger a rota /orders

--- backend/server.ts
+++ backend/server.ts
@@ -3,7 +3,8 @@ import { Express } from 'express'
 
 import * as fs from 'fs'
 import * as https from 'https'
-import { handleAuthentication } from './auth';
+import { handleAuthentication } from './auth'
+import { handleAuthorization } from './authz'
 
 const server: Express = jsonServer.create()
 const router = jsonServer.router('db.json')
@@ -18,6 +19,7 @@ server.use(jsonServer.bodyParser)
 
 //Middleware para login
 server.post('/login', handleAuthentication)
+server.use('/orders', handleAuthorization)
 
 // Use default router
 server.use(router)
