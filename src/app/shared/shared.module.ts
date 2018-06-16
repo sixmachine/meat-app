3b51609beee1aabefcbf5444b462092c4268b1b2
Unknown <arthur_psena@hotmail.com>
2018-06-16 20:36:51 -0300

    Aula 118
    Criei uma classe de serviço pra bater no serviço de login e estou armazenando no usuário retornado nessa mesma classe.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,6 +9,7 @@ import { OrderService } from "../order/order.service";
 import { RestaurantsService } from "../restaurants/restaurants.service";
 import { SnackbarComponent } from './messages/snackbar/snackbar.component';
 import { NotificationService } from "./messages/notification.service";
+import { LoginService } from "../security/login/login.service";
 
 @NgModule({
     declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
@@ -21,7 +22,7 @@ export class SharedModule {
     static forRoot(): ModuleWithProviders {
         return {
             ngModule: SharedModule,
-            providers: [ShoppingCartService, OrderService, RestaurantsService, NotificationService]
+            providers: [ShoppingCartService, OrderService, RestaurantsService, NotificationService, LoginService]
         }
     }
 
