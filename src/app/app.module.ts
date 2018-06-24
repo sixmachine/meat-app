fdb6343f91f9a45d638527af5cec9b409220c076
Unknown <arthur_psena@hotmail.com>
2018-06-24 19:59:34 -0300

    Aula 128
    Atualização do angular 4 para o angular 6. Remoção da biblioteca de internacionalização do 'intl' de polyfills. Ajuste no arquivo angular.json para ler os scripts e os styles corretamente. Ajuste na propriedade na hora de gerar o build para produção.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,11 @@ import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { LocationStrategy, HashLocationStrategy, registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt'
+
+registerLocaleData(localePt, 'pt')
+
 import { LoginComponent } from './security/login/login.component';
 import { UserDetailComponent } from './header/user-detail/user-detail.component';
 import { ApplicationErrorHandler } from './app.error-handler';
@@ -51,7 +55,7 @@ import { ApplicationErrorHandler } from './app.error-handler';
   ],
   providers: [
     { provide: LocationStrategy, useClass: HashLocationStrategy },
-    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: LOCALE_ID, useValue: 'pt' },
     { provide: ErrorHandler, useClass: ApplicationErrorHandler }],
   bootstrap: [AppComponent]
 })
